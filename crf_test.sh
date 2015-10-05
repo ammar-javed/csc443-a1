b77bc5b8abf6f8735c066dbb0ff14c1c7722454d
Ammar Javed <ammarj965@gmail.com>
2015-10-05 11:47:54 -0400

    Added bash script to automate testing of 10 block sizes

---
+++ crf_test.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Currently 500 MB
+BYTES_TO_WRITE=524288000
+
+#4KB 8KB 16KB 32KB 64KB 256KB 512KB 1MB 2MB 3MB
+#block_sizes=(4096 8192 16384 32768 65536 262144 524288 1048576 2097152 3145728)
+
+block_names=("4KB" "32KB" "64KB" "256KB" "512KB" "1MB" "1.5MB" "2MB" "2.5MB" "3MB")
+block_sizes=(4096 32768 65536 262144 524288 1048576 1572864 2097152 2621440 3145728)
+
+b=0
+for i in ${block_sizes[@]}
+do
+   
+   echo "Runnning Block Size = ${block_names[$b]} ( ${i} bytes) test...\n"
+   bin/create_random_file test $BYTES_TO_WRITE ${i}
+   echo "-----\n"
+
+   b=`expr ${b} + 1`
+
+done
